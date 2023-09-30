7d1aa85e96b6bef5d3eb731168fb946ffd5d75c7
Bitl <bitlblox@gmail.com>
2023-09-30 16:47:20 -0700

    how many times do i have to fix linux scripts in all my mods

--- build_fr_linux.sh
+++ build_fr_linux.sh
@@ -8,4 +8,5 @@ sudo chmod -R +x devtools/bin
 sudo chmod -R +x devtools/bin/linux
 sudo chmod +x createfirefightreloadedproject
 sudo bash createfirefightreloadedproject
+sudo make -f firefight.mak clean
 sudo make -f firefight.mak
